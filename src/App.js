4b85bc0231ce5014dac7a8b021ee6442ef50c9c1
IsabelleLof <104595550+IsabelleLof@users.noreply.github.com>
2023-01-14 22:26:15 +0100

    codetest for neovici junior frontend, hardcoded 3 drinks and have maped them and used props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const DRINKS = [
+  {
+    strDrinkThumb: 'https:www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg',
+    strDrink: 'Mojitio',
+    strInstructions: 'Isabelle of the glass with the lime slice to make the salt stick to it. Take care to moisten only the outer rim and sprinkle the salt on it. The salt should present to the lips of the imbiber and never mix into the cocktail. Shake the other ingredients with ice, then carefully pour into the glass.'
+  },
+  {
+    strDrinkThumb: 'https:\/\/www.thecocktaildb.com\/images\/media\/drink\/5noda61589575158.jpg',
+    strDrink: 'Manhattan',
+    strInstructions: 'Benji was here of the glass with the lime slice to make the salt stick to it. Take care to moisten only the outer rim and sprinkle the salt on it. The salt should present to the lips of the imbiber and never mix into the cocktail. Shake the other ingredients with ice, then carefully pour into the glass.'
+  },
+  {
+    strDrinkThumb: 'https:\/\/www.thecocktaildb.com\/images\/media\/drink\/5noda61589575158.jpg',
+    strDrink: 'Margarita',
+    strInstructions: 'Rub the rim of the glass with the lime slice to make the salt stick to it. Take care to moisten only the outer rim and sprinkle the salt on it. The salt should present to the lips of the imbiber and never mix into the cocktail. Shake the other ingredients with ice, then carefully pour into the glass.'
+  },
+  ];
+
+  const Card = ({drink: {strDrinkThumb, strDrink, strInstructions}}) => {
+    return(
+        <div>
+          <div>
+            <img alt="" src={strDrinkThumb} />
+            <p>{strDrink}</p>
+            <p>{strInstructions}</p>
+          </div>
+          <button>Add to shoppinglist</button>  
+        </div>
+
+    )
 }
 
-export default App;
+
+const App = () => (
+  <div>
+    {DRINKS.map((drink, i) => {
+      if (i < 3) {
+        return <Card drink={drink} />}
+    })}
+  </div>
+
+)
+
+export default App
